a37e36f81a9d410fb1486edbafbc7c9452fa7a32
Gabriel Stone <gabrielstone@Gabriels-MacBook-Air.local>
2022-10-19 17:59:34 +0100

    grievous

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import logo from './logo.svg';
+import grevious from '../src/grevious.png';
 import './App.css';
+//import { isFocusable } from '@testing-library/user-event/dist/utils';
+
+/*
+function isLogo() {
+  let src;
+  if (grievousHello) {
+    src = grevious;
+  } else if (grievousGoodbye) {
+    src = logo;
+  }
+  return src;
+}
+*/
+
+function grievousHello() {
+  const general = document.getElementById('general');
+  const generalFace = document.getElementsByClassName("App-logo");
+  general.style.color = 'white';
+  generalFace.src = grevious;
+}
+
+function grievousGoodbye() {
+  const general = document.getElementById('general');
+  general.style.color = '#e61d1d';
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={grevious} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          /*href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer"*/
         >
           Hello <span id="dot">•</span> there!
         </a>
+        <p onMouseOver={grievousHello} onMouseOut={grievousGoodbye} id="general">General Kenobi</p>
       </header>
     </div>
   );
